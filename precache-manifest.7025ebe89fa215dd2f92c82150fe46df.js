0eac95e19b923267936f221cace5e8b68e53a392
sharukabdul <sharukabdul786@gmail.com>
2024-03-23 20:02:07 +0530

    Updates

---
+++ precache-manifest.7025ebe89fa215dd2f92c82150fe46df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae893a1db4261a628391edf2a15e4ef3",
+    "url": "/custom-app/index.html"
+  },
+  {
+    "revision": "d2cd10b65d682fda5d0d",
+    "url": "/custom-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8ad3baddae782b6c6263",
+    "url": "/custom-app/static/js/2.bd1db438.chunk.js"
+  },
+  {
+    "revision": "be01e62d770977756b1e",
+    "url": "/custom-app/static/js/3.faef68d7.chunk.js"
+  },
+  {
+    "revision": "d2cd10b65d682fda5d0d",
+    "url": "/custom-app/static/js/main.0d83c183.chunk.js"
+  },
+  {
+    "revision": "cafb5377531a4cdbe4ca",
+    "url": "/custom-app/static/js/runtime~main.60f09eb6.js"
+  }
+]);
